ca69c1f1946cf0a121d61e887bd37e7984b0516c
kaschief <kaschiefj@gmail.com>
2018-12-15 14:14:10 +0100

    nav bar structure complete

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,12 @@ import React, { useState, useEffect } from 'react';
 const Nav = () => {
   return (
     <div className="Nav">
-      <h1>The Nav Bar</h1>
+      <div className="nav-logo">Logo</div>
+      <div className="nav-links">
+        <p>Purchase</p>
+        <p>My Orders</p>
+        <p>Sell</p>
+      </div>
     </div>
   );
 };
