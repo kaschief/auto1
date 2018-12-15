16811172c5a26789a2714dd9235990cba7eee7f2
kaschief <kaschiefj@gmail.com>
2018-12-15 14:33:06 +0100

    navbar working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from 'react';
 const Nav = () => {
   return (
     <div className="Nav">
-      <div className="nav-logo">Logo</div>
+      <div className="nav-logo">
+        <img className="logo" src="../../logo.png" alt="auto1-logo" />
+      </div>
       <div className="nav-links">
         <p>Purchase</p>
         <p>My Orders</p>
