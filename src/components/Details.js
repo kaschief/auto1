83a9e34755ec61e5210507681186460fee6995a1
kaschief <kaschiefj@gmail.com>
2018-12-15 18:50:33 +0100

    line height added to p

--- src/components/Details.js
+++ src/components/Details.js
@@ -9,7 +9,7 @@ const Details = () => {
         <div className="first">
           <h1>Chrysler Crossfire</h1>
           <p className="stock">Stock # 61184 - 152.263 KM - Petrol - Yellow</p>
-          <p>
+          <p className="car-status">
             This car is currently available and can be delivered as soon as
             tomorrow morning. Please be aware that delivery times shown in this
             page are not definitive and may change due to bad weather
