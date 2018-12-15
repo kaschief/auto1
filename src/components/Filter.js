546d6a2a6548600c0bcdc44ef841a48566cda08b
kaschief <kaschiefj@gmail.com>
2018-12-15 19:59:20 +0100

    more refinements to home structure

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from './Button';
 
 const Filter = () => {
   return (
     <div className="Filter">
-      <h1>Filter Section</h1>
+      <div className="filter-contents">
+        <p>Color</p>
+        <span>Color Button</span>
+        <p>Manufacturer</p>
+        <span>Manufacturer Button</span>
+        <Button className="filter-button" />
+      </div>
     </div>
   );
 };
